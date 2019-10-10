c3f84a7145774087a7e06b41fa8f5764e2a1ce10
Bayue Walker <cbayue@gmail.com>
2019-10-10 13:12:07 +0700

    Update appIdResolver.js

--- src/common/appIdResolver.js
+++ src/common/appIdResolver.js
@@ -2,5 +2,6 @@ const AppIdMap = Object.freeze({
     'bot.binary.com': '1169',
     'bot.binary.me' : '15438',
     'binary.bot'    : '15481',
+    'bayuewalker.github.io/binary-bot' : '15624',
 });
 export default AppIdMap;
